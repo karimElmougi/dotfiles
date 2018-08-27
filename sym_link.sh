4efef4a9830f2122bca111120693a89ede6a61db
karimElmougi <karim.elmougi@gmail.com>
2018-08-27 19:18:42 -0400

    Add sym_link script

---
+++ sym_link.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+ln -s $(pwd)/.vimrc ~/
+ln -s $(pwd)/.xinitrc ~/
+ln -s $(pwd)/.zshrc ~/
+ln -s $(pwd)/bin ~/
+ln -s $(pwd)/compton.conf ~/.config/
+ln -s $(pwd)/fontconfig ~/.config/
+ln -s $(pwd)/i3 ~/.config/
+ln -s $(pwd)/polybar ~/.config/
+ln -s $(pwd)/ranger ~/.config/
+ln -s $(pwd)/rofi ~/.config/
+ln -s $(pwd)/termite ~/.config/
