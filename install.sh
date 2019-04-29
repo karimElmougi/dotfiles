45ee6da99caec246929b0b44a396419c1d79a002
karim <karim.elmougi@gmail.com>
2019-04-29 00:59:19 -0400

    Clean up config paths and install script

--- install.sh
+++ install.sh
@@ -1,14 +1,9 @@
 #! /usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-ln -sf $DIR/../.vimrc ~/
-ln -sf $DIR/../.xinitrc ~/
-ln -sf $DIR/../.zshrc ~/
-ln -sf $DIR/../bin ~/
-ln -sf $DIR/../compton.conf ~/.config/
-ln -sf $DIR/../fontconfig ~/.config/
-ln -sf $DIR/../ranger ~/.config/
-ln -sf $DIR/../rofi ~/.config/
+for f in $DIR/dot/.*; do if [[ -f $f ]]; then ln -sf $f ~/; fi; done
+ln -sf $DIR/bin ~/
+ln -sf $DIR/config/* ~/.config/
 
 echo '%wheel ALL=NOPASSWD: /usr/bin/xbps-install -S' | sudo tee -a /etc/sudoers 
 echo '%wheel ALL=NOPASSWD: /usr/bin/reboot' | sudo tee -a /etc/sudoers 
@@ -17,3 +12,4 @@ echo '%wheel ALL=NOPASSWD: /usr/bin/runit-init 0' | sudo tee -a /etc/sudoers
 cd ~/Pictures
 wget https://i.imgur.com/HktehdS.png
 feh --bg-fill ~/Pictures/HktehdS.png
+
