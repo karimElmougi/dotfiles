cd1372c0239966ee88ac3a5013eb80ecae6e5067
karim <karim.elmougi@gmail.com>
2019-05-01 22:56:07 -0400

    Add awesome-wm-widgets

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ for f in $DIR/dot/.*; do if [[ -f $f ]]; then ln -sf $f ~/; fi; done
 ln -sf $DIR/bin ~/
 ln -sf $DIR/config/* ~/.config/
 
-echo '%wheel ALL=NOPASSWD: /usr/bin/xbps-install -S' | sudo tee -a /etc/sudoers 
 echo '%wheel ALL=NOPASSWD: /usr/bin/reboot' | sudo tee -a /etc/sudoers 
 echo '%wheel ALL=NOPASSWD: /usr/bin/runit-init 0' | sudo tee -a /etc/sudoers 
 
