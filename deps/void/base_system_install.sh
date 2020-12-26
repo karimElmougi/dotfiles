a3f298a2a6815e15570fe8b076209bc9e441e7cf
Karim Elmougi <karim.elmougi@gmail.com>
2020-12-26 14:08:55 -0500

    Remove basically everything

--- deps/void/base_system_install.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-./xbps-install-list packages/base_system && \
-xdg-user-dirs-update && \
-chsh -s /bin/zsh && \
-git config --global user.email "karim.elmougi@gmail.com" && \
-git config --global user.name "karim" && \
-sudo reboot
-
--- install.sh
+++ install.sh
@@ -1,14 +1,8 @@
 #! /usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
 for f in $DIR/dot/.*; do if [[ -f $f ]]; then ln -sf $f ~/; fi; done
-ln -sf $DIR/bin ~/
+
 ln -sf $DIR/config/* ~/.config/
-
-echo '%wheel ALL=NOPASSWD: /usr/bin/reboot' | sudo tee -a /etc/sudoers 
-echo '%wheel ALL=NOPASSWD: /usr/bin/runit-init 0' | sudo tee -a /etc/sudoers 
-
-cd ~/Pictures
-wget https://i.imgur.com/HktehdS.png
-feh --bg-fill ~/Pictures/HktehdS.png
 
