308491f4a395eb06212de75a1023e5f9c6fc0c69
Karim Elmougi <karim.elmougi@gmail.com>
2020-12-26 14:49:41 -0500

    Add vim-plug to install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 export PATH="$HOME/.cargo/bin:$PATH"
 cargo install bat exa loc bottom du-dust fd-find ripgrep starship
 
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 for f in $DIR/dot/.*; do if [[ -f $f ]]; then ln -sf $f ~/; fi; done
