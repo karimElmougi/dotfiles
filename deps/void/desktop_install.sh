42f18545e313067a98189f63e47dac28fe70d8ca
Karim Elmougi <karim.elmougi@gmail.com>
2020-12-26 14:08:55 -0500

    Remove basically everything

--- deps/void/desktop_install.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-./xbps-install-list packages/desktop && \
-sudo ln -s /etc/sv/dbus /var/service/ && \
-sudo ln -s /etc/sv/alsa /var/service/
-
-sudo reboot
-
--- deps/void/materia-theme_install.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-git clone --dept 1 https://github.com/nana-4/materia-theme
-cd materia-theme
-sudo ./install.sh
-cd ..
-rm -rf materia-theme
-
