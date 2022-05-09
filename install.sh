c8da1393c99ce86cbe84cf2a37c50a6d58509aec
karim <karim.elmougi@gmail.com>
2022-05-09 10:42:00 -0400

    Add pkgin config

--- install.sh
+++ install.sh
@@ -1,8 +1,45 @@
 #! /usr/bin/env bash
 
+if !command -v git &> /dev/null then
+  echo "git not installed!"
+  exit 1
+fi
+
+if !command -v cargo &> /dev/null then
+  echo "Cargo not installed!"
+  exit 1
+fi
+
+if [[ `uname` == "Darwin" ]] && !command -v pkgin; then
+    echo "pkgin not installed!"
+    exit 1
+fi
+
 DIR=$(git rev-parse --show-toplevel)
 
-## Install oh-my-zsh, fast-syntax-highlighting and zsh-autosuggestions manually
+if [ -d $HOME/.oh-my-zsh ]; then
+  echo "oh-my-zsh is already installed, skipping"
+else
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+export PATH="$HOME/.cargo/bin:$PATH"
+cargo install bat exa bottom du-dust fd-find ripgrep starship git-delta tealdeer tokei sd
+
+if [[ `uname` == "Darwin" ]]; then
+  sudo pkgin install \
+    git \
+    gh \
+    go \
+    gopls \
+    jq \
+    tmux \
+    the_silver_searcher \
+    zsh-syntax-highlighting \
+    zsh-autosuggestions
+fi
+
+mkdir -p $HOME/.bin
 
 mkdir -p $HOME/.fonts && \
 for f in $DIR/fonts/*; do if [[ -f $f ]]; then ln -sf "$f" $HOME/.fonts; fi; done
--- zshrc
+++ zshrc
@@ -7,18 +7,35 @@ export ZSH=$HOME/.oh-my-zsh
 plugins=(fast-syntax-highlighting zsh-autosuggestions)
 source $ZSH/oh-my-zsh.sh
 
-CARGO_BIN=$HOME/.cargo/bin
+if [[ `uname` == "Darwin" ]]; then
+  PKGIN_BIN=/opt/pkg/bin
+  PKGIN_SBIN=/opt/pkg/sbin
+  export PATH="$PKGIN_BIN:$PKGIN_SBIN:$PATH"
+fi
+
 HOME_BIN=$HOME/.bin
+
+if [ -d $HOME_BIN/zig ]; then
+  ZIG_PATH=$HOME_BIN/zig
+  export PATH="$ZIG_PATH:$PATH"
+fi
+
+if [ -d $HOME_BIN/neovim ]; then
+  NEOVIM_PATH=$HOME_BIN/neovim/bin
+  export PATH="$NEOVIM_PATH:$PATH"
+fi
+
+CARGO_BIN=$HOME/.cargo/bin
 export GOPATH=$HOME/dev/go
 export GOBIN=$GOPATH/bin
-export PATH="$HOME_BIN:$CARGO_BIN:$GOBIN:$PATH"
+
+export PATH="$CARGO_BIN:$GOBIN:$PATH"
 
 alias vim=nvim
 alias ls='exa --group-directories-first'
 alias cat=bat
 
 export EDITOR=nvim
-export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -f -g ""'
 
 #neofetch
 eval "$(starship init zsh)"
