c79b1cda9d3054e18b0646e84ef100390ffb3f35
karim <karim.elmougi@gmail.com>
2022-05-14 18:09:34 -0400

    Fix install script

--- install.sh
+++ install.sh
@@ -1,29 +1,38 @@
 #! /usr/bin/env bash
 
-if !command -v git &> /dev/null then
+if ! command -v git &> /dev/null 
+then
   echo "git not installed!"
   exit 1
 fi
 
-if !command -v cargo &> /dev/null then
+if ! command -v cargo &> /dev/null 
+then
   echo "Cargo not installed!"
   exit 1
 fi
 
-if [[ `uname` == "Darwin" ]] && !command -v pkgin; then
-  if !command -v pkgin; then
+if [[ `uname` == "Darwin" ]]; 
+then
+  if ! command -v pkgin; 
+  then
     echo "pkgin not installed!"
     exit 1
   fi
-  if !command -v brew; then
+  if ! command -v brew; 
+  then
     echo "brew not installed!"
     exit 1
   fi
+else
+  echo "Script not configured for OS, aborting"
+  exit 1
 fi
 
 DOTFILES=$(git rev-parse --show-toplevel)
 
-if [ -d $HOME/.oh-my-zsh ]; then
+if [ -d $HOME/.oh-my-zsh ]; 
+then
   echo "oh-my-zsh is already installed, skipping"
 else
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -32,7 +41,8 @@ fi
 export PATH="$HOME/.cargo/bin:$PATH"
 cargo install bat exa bottom du-dust fd-find ripgrep starship git-delta tealdeer tokei sd
 
-if [[ `uname` == "Darwin" ]]; then
+if [[ `uname` == "Darwin" ]]; 
+then
   sudo pkgin install \
     git \
     gh \
@@ -48,9 +58,11 @@ fi
 
 mkdir -p $HOME/.bin
 
+echo "Installing fonts..."
 mkdir -p $HOME/.fonts && \
 for f in $DOTFILES/fonts/*; do if [[ -f $f ]]; then ln -sf "$f" $HOME/.fonts; fi; done
 
+echo "Creating symbolic links to config files..."
 mkdir -p $HOME/.config && \
 for f in $DOTFILES/config/*; do ln -sf "$f" $HOME/.config; done
 
