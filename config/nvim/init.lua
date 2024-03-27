91f8e87ffed6a44a6d63690e43158f7dd1aadffa
karim <karim.elmougi@gmail.com>
2024-03-26 23:27:05 -0400

    Replace some plugins with mini

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -14,9 +14,13 @@ vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 require('packer').startup(function(use)
   use 'wbthomason/packer.nvim' -- Package manager
 
-  use { 'terrortylor/nvim-comment', config = function() require('nvim_comment').setup() end } -- Comment visual regions/lines, mapped to gcc and gc by default
+  use { 'echasnovski/mini.nvim', branch = 'stable' } -- Bunch of mini plugins
+
   use { 'windwp/nvim-autopairs', config = function() require('nvim-autopairs').setup() end } -- Automatically close brackets
   use { 'lukas-reineke/indent-blankline.nvim', tag = 'v2.20.6' } -- Add indentation guides even on blank lines
+
+  -- Remove with nvim 0.10
+  use {'ojroques/nvim-osc52'} -- Clipboard that works across SSH, same method as Zellij
 
   -- UI
   use { 'nvim-telescope/telescope.nvim', tag = '0.1.6', requires = { 'nvim-lua/plenary.nvim' } } -- UI to select things (files, grep results, open buffers...)
@@ -24,13 +28,11 @@ require('packer').startup(function(use)
   use 'numToStr/Navigator.nvim' -- Splits navigation
   use 'akinsho/toggleterm.nvim' -- Toggleable terminal
   use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } } -- Add git related info in the signs columns and popups
-  use { 'freddiehaddad/feline.nvim', tag = 'v1.4.0' } -- Status line
 
   -- LSP & Completion
   use 'nvim-treesitter/nvim-treesitter' -- Highlight, edit, and navigate code using a fast incremental parsing library
   use 'nvim-treesitter/nvim-treesitter-textobjects' -- Additional textobjects for treesitter
   use { 'neovim/nvim-lspconfig', tag = 'v0.1.7' } -- Collection of configurations for built-in LSP client
-  use { 'ms-jpq/coq_nvim', commit = 'a290446adad540d780e87d7fa8ef86bb2fdc2951' } -- Autocompletion plugin
 
   -- Style
   use { 'norcalli/nvim-colorizer.lua', config = function() require('colorizer').setup() end } -- Show color blocks around color codes
@@ -86,6 +88,24 @@ vim.o.mouse = 'a'
 
 --Share clipboard with system
 vim.o.clipboard='unnamedplus'
+
+local function copy(lines, _)
+  require('osc52').copy(table.concat(lines, '\n'))
+end
+
+local function paste()
+  return {vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('')}
+end
+
+vim.g.clipboard = {
+  name = 'osc52',
+  copy = {['+'] = copy, ['*'] = copy},
+  paste = {['+'] = paste, ['*'] = paste},
+}
+
+-- Now the '+' register will copy to system clipboard using OSC52
+-- vim.keymap.set('n', '<leader>c', '"+y')
+-- vim.keymap.set('n', '<leader>cc', '"+yy')
 
 --Enable break indent
 vim.o.breakindent = true
@@ -158,6 +178,15 @@ vim.g.indent_blankline_buftype_exclude = { 'terminal', 'nofile' }
 vim.g.indent_blankline_char_highlight = 'LineNr'
 vim.g.indent_blankline_show_trailing_blankline_indent = false
 
+-- Mini
+require('mini.completion').setup()
+vim.keymap.set('i', '<Tab>',   [[pumvisible() ? "\<C-n>" : "\<Tab>"]],   { expr = true })
+vim.keymap.set('i', '<S-Tab>', [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]], { expr = true })
+require('mini.comment').setup()
+require('mini.indentscope').setup() -- view indent levels with an animate line
+require('mini.notify').setup()
+require('mini.statusline').setup()
+
 -- Gitsigns
 require('gitsigns').setup {
   signs = {
@@ -172,6 +201,7 @@ require('gitsigns').setup {
 -- -- Telescope
 require('telescope').setup {
   defaults = {
+    generic_sorter = require('mini.fuzzy').get_telescope_sorter,
     mappings = {
       i = {
         ['<C-u>'] = false,
@@ -282,252 +312,12 @@ nvim_lsp.rust_analyzer.setup {
   }
 }
 
-local coq = require("coq")
-nvim_lsp.rust_analyzer.setup(coq.lsp_ensure_capabilities())
+nvim_lsp.gopls.setup{
+  on_attach = on_attach
+}
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
-
--- feline config
-local colors = {
-    bg = '#282c34',
-    fg = '#abb2bf',
-    yellow = '#e0af68',
-    cyan = '#56b6c2',
-    darkblue = '#081633',
-    green = '#98c379',
-    orange = '#d19a66',
-    violet = '#a9a1e1',
-    magenta = '#c678dd',
-    blue = '#61afef',
-    red = '#e86671'
-}
-
-local vi_mode_colors = {
-    NORMAL = colors.green,
-    INSERT = colors.red,
-    VISUAL = colors.magenta,
-    OP = colors.green,
-    BLOCK = colors.blue,
-    REPLACE = colors.violet,
-    ['V-REPLACE'] = colors.violet,
-    ENTER = colors.cyan,
-    MORE = colors.cyan,
-    SELECT = colors.orange,
-    COMMAND = colors.green,
-    SHELL = colors.green,
-    TERM = colors.green,
-    NONE = colors.yellow
-}
-
-local function file_osinfo()
-    local os = vim.loop.os_uname().sysname
-    if os == 'Linux' then
-        return ' LINUX'
-    elseif os == 'Darwin' then
-        return ' MAC'
-    else
-        return ' WINDOWS'
-    end
-end
-
-local lsp = require 'feline.providers.lsp'
-local vi_mode_utils = require 'feline.providers.vi_mode'
-
--- LuaFormatter off
-
-local comps = {
-    vi_mode = {
-        left = {
-            provider = function()
-              return '  ' .. vi_mode_utils.get_vim_mode()
-            end,
-            hl = function()
-                local val = {
-                    name = vi_mode_utils.get_mode_highlight_name(),
-                    fg = vi_mode_utils.get_mode_color(),
-                    -- fg = colors.bg
-                }
-                return val
-            end,
-            right_sep = ' '
-        },
-        right = {
-            -- provider = '▊',
-            provider = '' ,
-            hl = function()
-                local val = {
-                    name = vi_mode_utils.get_mode_highlight_name(),
-                    fg = vi_mode_utils.get_mode_color()
-                }
-                return val
-            end,
-            left_sep = ' ',
-            right_sep = ' '
-        }
-    },
-    file = {
-        info = {
-            provider = {
-              name = 'file_info',
-              opts = {
-                type = 'relative',
-                file_readonly_icon = '  ',
-                -- file_readonly_icon = '  ',
-                -- file_readonly_icon = '  ',
-                -- file_readonly_icon = '  ',
-                -- file_modified_icon = '',
-                file_modified_icon = '',
-                -- file_modified_icon = 'ﱐ',
-                -- file_modified_icon = '',
-                -- file_modified_icon = '',
-                -- file_modified_icon = '',
-              }
-            },
-            hl = {
-                fg = colors.blue,
-                style = 'bold'
-            }
-        },
-        encoding = {
-            provider = 'file_encoding',
-            left_sep = ' ',
-            hl = {
-                fg = colors.violet,
-                style = 'bold'
-            }
-        },
-        type = {
-            provider = 'file_type'
-        },
-        os = {
-            provider = file_osinfo,
-            left_sep = ' ',
-            hl = {
-                fg = colors.violet,
-                style = 'bold'
-            }
-        },
-        position = {
-            provider = 'position',
-            left_sep = ' ',
-            hl = {
-                fg = colors.cyan,
-                -- style = 'bold'
-            }
-        },
-    },
-    left_end = {
-        provider = function() return '' end,
-        hl = {
-            fg = colors.bg,
-            bg = colors.blue,
-        }
-    },
-    line_percentage = {
-        provider = 'line_percentage',
-        left_sep = ' ',
-        hl = {
-            style = 'bold'
-        }
-    },
-    scroll_bar = {
-        provider = 'scroll_bar',
-        left_sep = ' ',
-        hl = {
-            fg = colors.blue,
-            style = 'bold'
-        }
-    },
-    lsp = {
-        name = {
-            provider = 'lsp_client_names',
-            -- left_sep = ' ',
-            right_sep = ' ',
-            -- icon = '  ',
-            icon = '慎',
-            hl = {
-                fg = colors.yellow
-            }
-        }
-    },
-    git = {
-        branch = {
-            provider = 'git_branch',
-            icon = ' ',
-            -- icon = ' ',
-            left_sep = ' ',
-            hl = {
-                fg = colors.violet,
-                style = 'bold'
-            },
-        },
-        add = {
-            provider = 'git_diff_added',
-            hl = {
-                fg = colors.green
-            }
-        },
-        change = {
-            provider = 'git_diff_changed',
-            hl = {
-                fg = colors.orange
-            }
-        },
-        remove = {
-            provider = 'git_diff_removed',
-            hl = {
-                fg = colors.red
-            }
-        }
-    }
-}
-
-local components = {
-  active = {},
-  inactive = {},
-}
-
-table.insert(components.active, {})
-table.insert(components.active, {})
-table.insert(components.active, {})
-table.insert(components.inactive, {})
-table.insert(components.inactive, {})
-table.insert(components.inactive, {})
-
-table.insert(components.active[1], comps.vi_mode.left)
-table.insert(components.active[1], comps.file.info)
-table.insert(components.active[1], comps.git.branch)
-table.insert(components.active[1], comps.git.add)
-table.insert(components.active[1], comps.git.change)
-table.insert(components.active[1], comps.git.remove)
-
-table.insert(components.inactive[1], comps.vi_mode.left)
-table.insert(components.inactive[1], comps.file.info)
-
-table.insert(components.active[3], comps.lsp.name)
-table.insert(components.active[3], comps.file.os)
-table.insert(components.active[3], comps.file.position)
-table.insert(components.active[3], comps.line_percentage)
-table.insert(components.active[3], comps.scroll_bar)
-table.insert(components.active[3], comps.vi_mode.right)
-
-
-require('feline').setup {
-    colors = { bg = colors.bg, fg = colors.fg },
-    components = components,
-    vi_mode_colors = vi_mode_colors,
-    force_inactive = {
-        filetypes = {
-            'packer',
-            'NvimTree',
-            'fugitive',
-            'fugitiveblame'
-        },
-        buftypes = {'terminal'},
-        bufnames = {}
-    }
-}
 
 require('toggleterm').setup({
   -- size can be a number or function which is passed the current terminal
@@ -554,5 +344,3 @@ vim.api.nvim_set_keymap('n', '˚', "<CMD>lua require('Navigator').up()<CR>", { n
 vim.api.nvim_set_keymap('n', '¬', "<CMD>lua require('Navigator').right()<CR>", { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap('n', '<C-n>', '<Cmd>Neotree toggle=true focus<CR>', { noremap = true, silent = true })
-
-vim.api.nvim_create_autocmd('VimEnter', { command = 'COQnow --shut-up' })
