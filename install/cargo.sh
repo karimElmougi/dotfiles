40b9764f4afd9b509a050e44ed4f48ac69b13693
karim <karim.elmougi@gmail.com>
2025-01-09 09:40:52 -0500

    Make install scripts executable

--- install/cargo.sh
+++ install/cargo.sh
@@ -2,4 +2,4 @@
 
 echo "Installing cargo packages"
 export PATH="$HOME/.cargo/bin:$PATH"
-cargo install bat eza bottom du-dust fd-find ripgrep starship git-delta tealdeer tokei sd
+cargo install bat eza bottom du-dust fd-find ripgrep starship git-delta tealdeer tokei sd xh
