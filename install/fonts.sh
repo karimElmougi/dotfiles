854aff10a53710c66cf42385744ddc570eb49d97
karim <karim.elmougi@gmail.com>
2025-01-09 09:43:49 -0500

    Fix font install script

--- install/fonts.sh
+++ install/fonts.sh
@@ -1,11 +1,26 @@
 #! /usr/bin/env bash
 
 echo "Installing nerd fonts"
-git clone --depth 1 https://github.com/ryanoasis/nerd-fonts
-cd nerd-fonts \
-  && ./install.sh Iosevka \
-  && ./install.sh Inconsolata \
-  && ./install.sh Hack \
-  && ./install.sh FiraCode \
-  && ./install.sh Monaspace \
-  && cd ..
+if [[ `uname` != "Darwin" ]]; then
+    echo "Only Mac OS is currently supported"
+    exit 1
+fi
+
+set -x
+
+cd ~/Library/Fonts
+
+TAG=$(xh get https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | jq --raw-output '.tag_name')
+URL="https://github.com/ryanoasis/nerd-fonts/releases/download/$TAG"
+
+install() {
+    mkdir $1 && cd $1 && xh --download $URL/$1.zip
+    unzip $1.zip
+    rm $1.zip
+}
+
+install Monaspace
+# xh --download $URL/Iosevka.zip
+# xh --download $URL/Inconsolata.zip
+# xh --download $URL/Hack.zip
+# xh --download $URL/FiraCode.zip
