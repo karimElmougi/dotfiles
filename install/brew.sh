d9c30d878df03449b315025107466ebdc86270eb
karim <karim.elmougi@gmail.com>
2025-11-10 17:54:39 -0500

    Suppress brew detection output

--- install/brew.sh
+++ install/brew.sh
@@ -1,7 +1,6 @@
 #! /usr/bin/env bash
 
-
-if [[ `uname` == "Darwin" ]] && ! command -v brew; then
+if [[ `uname` == "Darwin" ]] && ! command -v brew > /dev/null 2>&1; then
     echo "Installing brew"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
