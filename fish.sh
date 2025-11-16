a6190b0b1ba3c82b24853396e4683cd85c6506b1
karim <karim.elmougi@gmail.com>
2025-11-15 21:10:27 -0500

    Add fish variable script

---
+++ fish.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env fish
+
+# Disable fish greeting
+set -U fish_greeting
+
+# Tide config
+set -U tide_right_prompt_items
+set -U tide_left_prompt_items context pwd git cmd_duration character
