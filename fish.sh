de3045f7b0cfb9482e2acec879965e8c6b6ba0f3
karim <karim.elmougi@gmail.com>
2025-11-17 11:10:58 -0500

    Add tide stuff to gitignore

--- fish.sh
+++ fish.sh
@@ -3,6 +3,11 @@
 # Disable fish greeting
 set -U fish_greeting
 
+set -U _fisher_plugins jorgebucaran/fisher
+
+fisher update
+fisher install IlanCosman/tide@v6
+
 # Tide config
 set -U tide_right_prompt_items
 set -U tide_left_prompt_items context pwd git cmd_duration character
