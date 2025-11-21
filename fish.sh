ae3f3d565e6866ca22ecd79023cab967ecfffaa5
karim <karim.elmougi@gmail.com>
2025-11-21 10:50:18 -0500

    Add tide config to fish.sh

--- fish.sh
+++ fish.sh
@@ -3,11 +3,15 @@
 # Disable fish greeting
 set -U fish_greeting
 
-set -U _fisher_plugins jorgebucaran/fisher
+if not contains jorgebucaran/fisher $_fisher_plugins
+    # First set up
+    set -U _fisher_plugins jorgebucaran/fisher
+    fisher install IlanCosman/tide@v6
+end
 
 fisher update
-fisher install IlanCosman/tide@v6
 
 # Tide config
+tide configure --auto --style=Lean --prompt_colors='True color' --show_time=No --lean_prompt_height='One line' --prompt_spacing=Compact --icons='Few icons' --transient=No
 set -U tide_right_prompt_items
 set -U tide_left_prompt_items context pwd git cmd_duration character
